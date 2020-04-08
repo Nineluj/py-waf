542eeacf5b308bad541c455ddf31ac5187a23dca
Julian Hirn <julian.hirn@protonmail.com>
2020-04-08 17:44:48 +0800

    cleaned up methods used to get POST working

--- waf/reverse_proxy.py
+++ waf/reverse_proxy.py
@@ -1,6 +1,7 @@
 from flask import request, Blueprint, current_app as app, redirect
 import requests
 from urllib.parse import urlparse
+from typing import List, Dict, Tuple
 
 EXCLUDED_HEADERS = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
 reverse_proxy = Blueprint('reverse_proxy', __name__)
@@ -11,7 +12,7 @@ def make_400():
     return "Record not found", 400
 
 
-def get_app_url(path):
+def get_app_url(path: str) -> str:
     """Resolve requested path into the address on the application server"""
     server_addr = app.config['server_addr']
     rest = ""
@@ -23,9 +24,21 @@ def get_app_url(path):
     return f"http://{server_addr}/{path}{rest}"
 
 
-def get_filtered_headers(resp: requests.Response):
+def filter_headers_app_request(headers) -> Dict[str, str]:
+    """Gets the headers from the client request that can be passed on to the application server"""
+    new_headers = {}
+
+    for name, value in headers.items():
+        if name.lower() not in EXCLUDED_HEADERS:
+            new_headers[name] = value
+
+    return new_headers
+
+
+def get_filtered_headers_client_response(resp: requests.Response) -> List[Tuple[str, str]]:
     """Gets the headers that don't include data specific for the proxied request from the Response"""
-    return [(name, value) for (name, value) in resp.raw.headers.items()
+    headers = resp.raw.headers
+    return [(name, value) for (name, value) in headers.items()
             if name.lower() not in EXCLUDED_HEADERS]
 
 
@@ -40,7 +53,7 @@ def proxy(path):
 
     if request.method == 'GET':
         resp = requests.get(url=app_url, allow_redirects=False)
-        headers = get_filtered_headers(resp)
+        headers = get_filtered_headers_client_response(resp)
 
         # We need to handle redirects correctly
         if resp.is_redirect:
@@ -53,9 +66,9 @@ def proxy(path):
         # Flask routes can accept tuple (content, status, headers)
         return resp.content, resp.status_code, headers
     elif request.method == "POST":
-        resp = requests.post(url=app_url, data=request.get_data())
-
-        return resp.content, resp.status_code, get_filtered_headers(resp)
+        app_request_headers = filter_headers_app_request(dict(request.headers))
+        resp = requests.post(url=app_url, data=request.get_data(), headers=app_request_headers)
+        return resp.content, resp.status_code, get_filtered_headers_client_response(resp)
     else:
         # TODO: Implement other methods
         return make_400()
