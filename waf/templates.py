2259ed95c8ee8d0e029f5a71ae0cfaa03242e03f
Lucas <>
2020-04-08 14:03:16 -0700

    Added documentation for templates.py

--- waf/templates.py
+++ waf/templates.py
@@ -1,6 +1,21 @@
 from waf.modules.sql_injection_check import sql_injection_check
 from waf.modules.test import test
 
+"""
+Templates = {
+    Key = path/page_url
+    Value = {
+            Key = form name
+            Value = [functions that input a single string and return a boolean]
+        }
+}
+
+Any function can be added to the template, see test.py for the most basic form
+- The input will always be the value in the POST message for that particular parameter
+- "" is the catch all, base security for any template-less page
+
+"""
+
 TEMPLATES = {
     "/www/SQL/sql1.php":
         {
