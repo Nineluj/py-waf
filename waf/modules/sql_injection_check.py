4976d63d8b57697934457a27d8904f0a6d98df09
Lucas <mccanna.l@husky.neu.edu>
2020-04-08 22:26:08 -0400

    Removed prints

--- waf/modules/sql_injection_check.py
+++ waf/modules/sql_injection_check.py
@@ -10,8 +10,6 @@ def sql_injection_check(content, allowed=None) -> bool:
     if allowed is None:
         allowed = []
     for invalid in [item for item in INVALID_INPUT if item not in allowed]:
-        print("\~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        print("Content:", content)
         if re.search(invalid, content):
             return False
     return True
