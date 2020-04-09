373c63214e102bb940fe91ef6524780c3803c138
Anand Hegde <anandrhegde21@gmail.com>
2020-04-09 03:41:41 -0700

    Refactor the configs

--- waf/modules/rate_limiter.py
+++ waf/modules/rate_limiter.py
@@ -1,18 +1,22 @@
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
+
 class RateLimiter(object):
     """Rate limiting"""
+
     def __init__(self, app):
         self.app = app
-        self.enabled = app.config['modules'].get('rate_limiter', True)
+        self.enabled = (app.config['modules'].get('rate_limiter', {'enabled': True})).get('enabled', True)
+        self.default_limits = (app.config['modules'].get('rate_limiter', {'default_limits': '5000/day;500/hour'})).get(
+            'default_limits', '5000/day;500/hour').split(';')
 
     def __call__(self):
         if self.enabled:
             Limiter(
                 self.app,
                 key_func=get_remote_address,
-                default_limits=["5000/day", "500/hour"]
+                default_limits=self.default_limits
             )
         else:
             """Do nothing"""
