523b187919705cddc493540c77c7fc64a5a833d3
Anand Hegde <anandrhegde21@gmail.com>
2020-04-10 01:53:17 -0700

    Add testing config for passwords

--- waf/modules/credential.py
+++ waf/modules/credential.py
@@ -9,38 +9,41 @@ from waf.custom_types.password_strength import PasswordStrength
 from waf.exceptions.credential_exception import CredentialException
 
 
-class Credential(object):
+class CredentialCheck(object):
     def __init__(self, app):
         self.app = app
-        self.enabled = (app.config['modules'].get('xss', {'enabled': True})).get('enabled', True)
-        self.filtered_urls = (app.config['modules'].get('xss', {'filtered_urls': {}})).get('filtered_urls', {})
-        self.password_strength = (
-            app.config['modules'].get('xss', {'password_strength': PasswordStrength.VERY_UNGUESSABLE})).get(
-            'password_strength', PasswordStrength.VERY_UNGUESSABLE)
-        if self.password_strength < PasswordStrength.VERY_GUESSABLE or self.password_strength > PasswordStrength.VERY_UNGUESSABLE:
+        self.enabled = (app.config['modules'].get('credential', {'enabled': True})).get('enabled', True)
+        self.filtered_urls = (app.config['modules'].get('credential', {'filtered_urls': {}})).get('filtered_urls', {})
+        self.password_strength = PasswordStrength((
+            app.config['modules'].get('credential', {'password_strength': PasswordStrength.VERY_UNGUESSABLE})).get(
+            'password_strength', PasswordStrength.VERY_UNGUESSABLE))
+        if self.password_strength.value < PasswordStrength.VERY_GUESSABLE.value or self.password_strength.value > PasswordStrength.VERY_UNGUESSABLE.value:
             self.password_strength = PasswordStrength.VERY_UNGUESSABLE
         self.password_check_api = 'https://api.pwnedpasswords.com/range/{}'
 
     def __call__(self, uri, data):
+        """Return True if this is matching a filtered_url"""
         if not self.enabled:
-            return data
+            return False
         else:
             """Start checking"""
             if uri not in self.filtered_urls:
-                return data
+                return False
             else:
                 fields = self.filtered_urls[uri]
                 for arg in data:
                     if arg in fields:
-                        if fields[arg] == CredentialType.EMAIL:
+                        print(fields[arg], fields[arg] == CredentialType.EMAIL, fields[arg] == CredentialType.PASSWORD)
+                        if CredentialType(fields[arg]) == CredentialType.EMAIL:
                             """Do something with the result here"""
                             self.__email_check(data[arg])
-                        elif fields[arg] == CredentialType.PASSWORD:
+                        elif CredentialType(fields[arg]) == CredentialType.PASSWORD:
                             """Do something with the result here"""
                             self.__password_check(data[arg], data)
                         else:
                             """Do nothing"""
                             pass
+                return True
 
     def __email_check(self, email):
         """Hit the have I been pwned api for emails"""
@@ -53,7 +56,7 @@ class Credential(object):
 
     def __is_password_pwned(self, password):
         """Has this password been pwned?"""
-        password_hash = sha1().update(password.encode()).hexdigest()
+        password_hash = sha1(password.encode()).hexdigest()
         res = requests.get(self.password_check_api.format(password_hash[:5]))
 
         if res.status_code != HTTPStatus.OK:
@@ -62,15 +65,18 @@ class Credential(object):
         for line in (res.content.decode().split("\r\n")):
             parts = line.split(":")
             if parts[0].lower() == password_hash[5:]:
-                if parts[1] > 500:
-                    raise CredentialException("Common password")
-                elif parts[1] > 10000:
+                times_found = int(parts[1])
+                if times_found > 10000:
                     raise CredentialException("Very common password")
+                elif times_found > 500:
+                    raise CredentialException("Common password")
+                elif times_found > 0:
+                    raise CredentialException("Breached password")
 
     def __is_password_unguessable(self, password, data):
         """Is this password easily guessable?"""
         result = zxcvbn(password, user_inputs=data)
-        if result['score'] < self.password_strength:
+        if result['score'] < self.password_strength.value:
             raise CredentialException(result['feedback'])
         else:
             """Do nothing"""
