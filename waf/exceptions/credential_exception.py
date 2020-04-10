36c18a77474de60f070594935eecd54ff16cb46e
Anand Hegde <anandrhegde21@gmail.com>
2020-04-10 00:39:10 -0700

    Add credential exception class

--- waf/exceptions/credential_exception.py
+++ waf/exceptions/credential_exception.py
@@ -1,6 +1,9 @@
+from pprint import pformat
+
+
 class CredentialException(Exception):
     def __init__(self, info):
         self.info = info
 
     def __str__(self):
-        return f"{self.info}"
+        return pformat(self.info)
