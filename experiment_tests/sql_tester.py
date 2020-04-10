199422fa67b5e8b02ff9b758cdac77c012f16678
Lucas <>
2020-04-09 17:38:10 -0700

    Added added more data sql_tests

--- experiment_tests/sql_tester.py
+++ experiment_tests/sql_tester.py
@@ -26,6 +26,25 @@ with open('sql_test.txt') as f:
             # Elapsed time for the transfer.
             print('Time: %f' % c.getinfo(c.TOTAL_TIME))
 
+fw = open(r"sql_short_pw_results.txt", "w+")
+with open('sql_short_pw.txt') as f:
+    for line in f:
+        if line != "" or line != "\n":
+            post_data = {'number': f'{line}', "submit": "Submit"}
+            postfields = urlencode(post_data)
+            c.setopt(pycurl.SSL_VERIFYPEER, 0)
+            c.setopt(c.POSTFIELDS, postfields)
+            c.perform()
+            # HTTP response code, e.g. 200.
+            print('Status: %d' % c.getinfo(c.RESPONSE_CODE))
+            if c.getinfo(c.RESPONSE_CODE) != 200:
+                fw.write(line.strip() + " | RESULT: " + str(c.getinfo(c.RESPONSE_CODE)) + "\r\n")
+            # if c.getinfo(c.RESPONSE_CODE) == 500:
+            #     exit(1)
+            # Elapsed time for the transfer.
+            print('Time: %f' % c.getinfo(c.TOTAL_TIME))
+            print('Time: %s' % line)
+
 fw = open(r"sql_test_false_positives_results.txt", "w+")
 with open('sql_test_false_positives.txt') as f:
     for line in f:
